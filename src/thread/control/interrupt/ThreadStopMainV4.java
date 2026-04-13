48ef2c2559d6593acfa3c7c152b1496ed298d977
thbins <joons0218@gmail.com>
2026-04-14 03:47:20 +0900

    학습: interrupt, isInterrupted, interrupted 메서드 학습

---
+++ src/thread/control/interrupt/ThreadStopMainV4.java
@@ -0,0 +1,41 @@
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV4 {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+
+        sleep(100); // 시간을 줄임
+        log("작업 중단 지시 thread.interrupt()");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                // 스레드가 인터럽트 상태라면 true를 반환하고, 해당 스레드의 인터럽트 상태를 false로 변경한다.
+                // 스레드가 인터럽트 상태가 아니라면 false를 반환하고, 해당 스레드의 인터럽트 상태를 변경하지 않는다.
+                log("작업 중");
+            }
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+
+            try {
+                log("자원 정리");
+                Thread.sleep(1000);
+                log("작업 종료");
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted());
+            }
+            log("작업 종료");
+        }
+    }
+}
