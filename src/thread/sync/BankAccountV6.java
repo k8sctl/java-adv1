f9c516d55ff8fba5e7875d9abe1aa6f0c90ece9e
thbins <joons0218@gmail.com>
2026-04-20 22:29:38 +0900

    학습: ReentrantLcok 활용 코드

---
+++ src/thread/sync/BankAccountV6.java
@@ -0,0 +1,62 @@
+package thread.sync;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV6 implements BankAccount {
+    private int balance;
+
+    private final Lock lock = new ReentrantLock();
+
+    public BankAccountV6(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        try {
+            if (!lock.tryLock(500, TimeUnit.MILLISECONDS)) {
+                log("[진입 실패] 이미 처리 중인 작업이 있습니다.");
+                return false; // 애초에 lock을 얻지 못 했기 때문에 unlock을 할 필요도 없다. 바로 false를 반환한다.
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            // == 임계 영역 시작 ==
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+
+            // 잔고가 출금액 보다 많으면, 진행
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000); // 출금에 걸리는 시간으로 가정
+            balance -= amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+            // == 임계 영역 종료 ==
+        } finally {
+            lock.unlock(); // ReentrantLcok 이용하여 lock 해제
+        }
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock(); // ReentrantLcok 이용하여 lock 걸기
+        try {
+            return balance;
+        } finally {
+            lock.unlock(); // ReentrantLcok 이용하여 lock 해제
+        }
+    }
+}
